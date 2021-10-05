1721c4726f4030fedd061125d15805cb6e5c6396
root <albertorosillogarcia@gmail.com>
2021-10-04 20:03:13 -0700

    low

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,24 +14,23 @@
  */
 int main(void)
 {
-  char letter = 'a';
-
-  while (letter <= 'z')
-    {
-      if (letter == 'q')
-	{
-	  letter++;
-	}
-      else if (letter == 'e')
-	{
-	  letter++;
-	}
-      else
-	{
-	  putchar(letter);
-	  letter++;
-	}
-    }
-  putchar('\n');
-  return (0);
+ char letter = 'a';
+ while (letter <= 'z')
+   {
+     if (letter == 'q')
+       {
+	 letter++;
+       }
+     else if (letter == 'e')
+       {
+	 letter++;
+       }
+     else
+       {
+	 putchar(letter);
+	 letter++;
+       }
+   }
+ putchar('\n');
+ return (0);
 }
