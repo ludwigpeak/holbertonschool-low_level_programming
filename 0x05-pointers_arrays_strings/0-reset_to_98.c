dca639ef4d60608667a55b05bea3b77e701073d5
root <albertorosillogarcia@gmail.com>
2021-10-11 13:46:19 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,13 @@
 /*
  * reset_to_98 - set value of int to 98
  * @n: address of value to change
- * Description: resets *n to 98
  * Return: nothing
  */
-
 void reset_to_98(int *n)
+/*
+ * Description: resets *n to 98
+*/
 {
-  *n=98;
+  *n = 98;
 }
 
