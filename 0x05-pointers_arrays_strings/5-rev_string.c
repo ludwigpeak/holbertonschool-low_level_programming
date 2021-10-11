9eae25ac75274cd64e2182b9255cfd07d1874920
root <albertorosillogarcia@gmail.com>
2021-10-11 15:01:54 -0700

    myschool

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+void rev_string(char *s)
+{
+	int i = 0;
+
+	int j = 0;
+
+	char temp[1000];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (; i > 0; i--)
+	{
+		temp[j] = s[i - 1];
+		j++;
+	}
+
+	while (i < j)
+	{
+		s[i] = temp[i];
+		i++;
+	}
+}
