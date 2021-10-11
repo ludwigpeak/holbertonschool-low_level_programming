314c360058881c24590ba37e377bcb9da91f8a4f
root <albertorosillogarcia@gmail.com>
2021-10-11 14:25:05 -0700

    swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,16 @@
 #define MAIN_H
 
 #include <stdio.h>
+/*
+ *Description: Resets n to 98
+*/
 
 void reset_to_98(int *n);
-   
+
+/*
+ *Descritpion: Swaps teh value of two int
+*/
+
+void swap_int(int *a, int *b);
+
 #endif /* MAIN_H */
