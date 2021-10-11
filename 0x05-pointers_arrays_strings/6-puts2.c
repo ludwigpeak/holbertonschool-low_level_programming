b3e02d4336f00dc32ea9a9efeeda7a5b52ef4492
root <albertorosillogarcia@gmail.com>
2021-10-11 15:19:19 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j += 2;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,4 +24,6 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+void puts2(char *str);
+
 #endif /* MAIN_H */
