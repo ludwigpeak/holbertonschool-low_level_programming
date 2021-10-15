5b66dcdb1f9bdbc6c0cc0ab8a337f1cbac69fdaa
root <albertorosillogarcia@gmail.com>
2021-10-15 12:11:29 -0700

    yeet2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,14 @@
 char *leet(char *c)
 {
   int i, j;
-  char *replace;
-  replace = "aAeEoOtTlL4433007711";
+  char *k;
+  k = "aAeEoOtTlL4433007711";
   for (i = 0; c[i] != '\0'; i++)
     {
       for (j = 0; j<= 9; j++)
 	{
-	  if (c[i] == replace[j])
-	    c[i] = replace[j + 10];
+	  if (c[i] == k[j])
+	    c[i] = k[j + 10];
 	 
 	}
     }
