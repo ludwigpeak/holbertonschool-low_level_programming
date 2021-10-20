c8ae2002af6db370828688d9cc718790e3379c8c
root <albertorosillogarcia@gmail.com>
2021-10-20 13:23:15 -0700

    strleng

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _strlen_recursion - returns length of string
- * @k string
+ * @k: string
  * Description: using recursion find the string length
  * Return: string length
  **/
 int _strlen_recursion(char *k)
 {
-  if (k[0] == '\0')
+if (k[0] == '\0')
 return(0);
-  if (k[1] == '\0')
-    {
-      return (1);
-    }
-  else
-    {
-      return (_strlen_recursion(&k[1]) + 1);
-    }
+if (k[1] == '\0')
+{
+return (1);
+}
+else
+{
+return (_strlen_recursion(&k[1]) + 1);
+}
 }
