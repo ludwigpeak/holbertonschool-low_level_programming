2f065a5a0659804ec3d93984a853b5d6541ad4c4
root <albertorosillogarcia@gmail.com>
2021-10-20 14:24:09 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,12 +10,13 @@ int prime_con(int n, int i)
 {
 if (n % i == 0)
 return (0);
-else if (n >= (i + 1) * (i *1))
+else if (n >= (i + 1) * (i * 1))
 return (prime_con(n, i + 1));
 return (1);
 }
 /**
  * is_prime_number - determine if n is a prime number
+ * prime_con - determine if n is a prime
  * @n: number to verify
  * Description: determine if n is a prime
  * Return: 1 if n is prime, 0 if not
