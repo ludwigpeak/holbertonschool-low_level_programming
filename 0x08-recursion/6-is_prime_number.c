e1ad06ce1f3a7ad7bd647cf7be9573d307b71e91
root <albertorosillogarcia@gmail.com>
2021-10-20 18:09:14 -0700

    primo

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- * prime_con - determine if n is a prime
+ * primo - determine if n is a prime
  * @n : test case
  * @i : recurring factor
  * Description :determine if n is a prime using two parameters
  * Return : 1 if n is a prime, 0 if not
  **/
-int prime_con(int n, int i)
+int primo(int n, int i)
 {
 if (n % i == 0)
 return (0);
 else if (n >= (i + 1) * (i * 1))
-return (prime_con(n, i + 1));
+return (primo(n, i + 1));
 return (1);
 }
 /**
  * is_prime_number - determine if n is a prime number
- * prime_con - determine if n is a prime
  * @n: number to verify
  * Description: determine if n is a prime
  * Return: 1 if n is prime, 0 if not
@@ -25,5 +24,5 @@ int is_prime_number(int n)
 {
 if (n < 2)
 return (0);
-return (prime_con(n, 2));
+return (primo(n, 2));
 }
