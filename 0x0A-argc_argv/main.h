49491ac853acbb6b5e98487fa7fe34121636bb95
root <albertorosillogarcia@gmail.com>
2021-10-26 12:54:06 -0700

    cambio

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int numcheck(char *s);
-  int main(int argc, char **argv);
+int main(int argc, char **argv);
+int numchecker(char *s);
 #endif /* MAIN_H */
