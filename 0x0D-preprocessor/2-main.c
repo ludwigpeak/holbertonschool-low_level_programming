3af6409ed304a9f343c8790f4932a31881d84af8
root <albertorosillogarcia@gmail.com>
2021-11-01 13:31:22 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program file, followed by a new line.
+ * Return: 0 
+ **/
+int main (void)
+{
+  printf("%s\n", __FILE__);
+  return (0)
+}
