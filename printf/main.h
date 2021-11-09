1974a1af5cf83634b2ec4027b5278ff447cb729b
root <albertorosillogarcia@gmail.com>
2021-11-09 12:43:50 -0800

    main

--- printf/main.h
+++ printf/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
 #include <stdio.h>
-int _printf(const char *format, ...);
+#include <stdarg.h>
+int __printf(const char *format, . . .);
+{
+va_list arg;
+int prueba;
+va_start (arg, format);
+prueba = vfprintf (stdout, format, arg);
+va_end (arg);
+return prueba; 
+}
+
 #endif /* MAIN_H */
